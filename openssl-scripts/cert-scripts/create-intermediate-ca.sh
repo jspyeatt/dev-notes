bc2fa70d5ef80663cba20d6616fa781a6d2ef6fa
John Pyeatt <john.pyeatt@singlewire.com>
2020-04-24 09:03:55 -0500

    cleaned up create-intermediate-ca.sh with templating

--- openssl-scripts/cert-scripts/create-intermediate-ca.sh
+++ openssl-scripts/cert-scripts/create-intermediate-ca.sh
@@ -1,45 +1,53 @@
 #!/bin/bash
 
-if [ $# -lt 1 ]
+
+scriptDir=$PWD
+baseCertDir=$HOME/my-certs
+configDir=${baseCertDir}/conf
+
+certRootDir=${baseCertDir}/rootCA
+certIntermediateDir=${baseCertDir}/intermediate
+keyFile=${certRootDir}/private/intermediate.key.pem
+certFile=${certRootDir}/certs/intermediate.cert.pem
+csrFile=${certIntermediateDir}/csr/intermediate.csr.pem
+if [ -e $certFile ]
 then
-  echo "ERROR: You must specify the directory of the .cnf files and the host server FQDN."
-  exit 1
+   echo "Error: $certFile already exists. If you really want to replace it you must remove it first"
+   exit 1
 fi
 
-configDir=$1
-
-# This will create all the artifacts of an intermediate CA and put them in the directory /tmp/certs/intermediateCA
-rootDir='/tmp/certs/intermediate'
-rm -rf $rootDir
-mkdir -p $rootDir
-cd $rootDir
+rm -rf $certIntermediateDir
+mkdir -p $certIntermediateDir
+cd $certIntermediateDir
 mkdir certs crl newcerts private csr
 touch index.txt
 echo 1000 > serial
 echo 1000 > crlnumber
 
-cd $rootDir
+sed -e "s!BASE_CERT_DIR!${certIntermediateDir}!" $scriptDir/openssl-intermediate.cnf.template > ${configDir}/openssl-intermediate.cnf
+
+cd $certIntermediateDir
 
 # create the private key - using RSA instead of AES256
-openssl genrsa -aes256 -out ${rootDir}/private/intermediate.key.pem -passout pass:changeMe 4096
+openssl genrsa -aes256 -out ${keyFile} -passout pass:changeMe 4096
 if [ $? -ne 0 ];then echo "ERROR creating private key"; exit 1; fi
 
 # create csr
-openssl req -config ${configDir}/openssl-intermediate.cnf -new -sha256 -key ${rootDir}/private/intermediate.key.pem -out ${rootDir}/csr/intermediate.csr.pem -passin pass:changeMe -subj "/C=US/ST=Wisconsin/L=Madison/O=Pyeatt/OU=Dev/CN=Intermediate CA"
+openssl req -config ${configDir}/openssl-intermediate.cnf -new -sha256 -key ${keyFile} -out ${csrFile} -passin pass:changeMe -subj "/C=US/ST=Wisconsin/L=Madison/O=Fake Certs/OU=Dev/CN=Intermediate CA"
 if [ $? -ne 0 ];then echo "ERROR creating intermediate signing request"; exit 1; fi
 
 # create intermediate certificate
-openssl ca -batch -config ${configDir}/openssl-root.cnf -extensions v3_intermediate_ca -days 3650 -notext -md sha256 -in ${rootDir}/csr/intermediate.csr.pem -out ${rootDir}/certs/intermediate.cert.pem -passin pass:changeMe
+openssl ca -batch -config ${configDir}/openssl-root.cnf -extensions v3_intermediate_ca -days 3650 -notext -md sha256 -in ${csrFile} -out ${certFile} -passin pass:changeMe
 if [ $? -ne 0 ];then echo "ERROR creating intermediate certificate"; exit 1; fi
 
 # verify the intermediate certificate
-openssl x509 -text -in ${rootDir}/certs/intermediate.cert.pem
+openssl x509 -text -in ${certFile}
 if [ $? -ne 0 ];then echo "ERROR verifying certificate"; exit 1; fi
 
 echo ""
 echo "***********************************************************************"
 echo "Intermediate Certificate verified"
-echo "public certificate      = ${rootDir}/certs/intermediate.cert.pem"
-echo "private key             = ${rootDir}/private/intermediate.key.pem"
+echo "public certificate      = ${certFile}"
+echo "private key             = ${keyFile}"
 echo "***********************************************************************"
 
